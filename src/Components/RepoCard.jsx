156413bd807d9f57bb48e0102451b0c77aa5ac60
Misal-Ambasta <misalambasta@gmail.com>
2020-12-01 13:36:03 +0530

    details styles added

--- src/Components/RepoCard.jsx
+++ src/Components/RepoCard.jsx
@@ -17,7 +17,7 @@ export default function RepoCard({ data }) {
                 {' '}
                 <img  src={avatar_url} />{' '}
             </div>
-            <div>
+            <div className={styles.details}>
                 <div>Repo name: {name}</div>
                 <div>Description: {description===null ? 'No description provided' : description}</div>
                 <button onClick={()=>handleRepoDetails()}>More details</button>
